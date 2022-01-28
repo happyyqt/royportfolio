022b7a3cb73fe8562c043f9b327c4d36d3d2dd0a
Qingtian <qingtianyu5@gmail.com>
2022-01-28 14:48:07 -0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# royportfolio
-
-<<<<<<< HEAD
-Hi! This is my portfolio! Feel free to visit [Roy-portfolio](https://happyyqt.github.io/royportfolio)
-=======
-This is the gh-page branch for displaying!
->>>>>>> 23ce009b75c3b6454d3209ca892478d125470013
---
+++ precache-manifest.a05da27b8e0c2ac991df895a9eec4a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64c0a03cc7f29a4bf030",
+    "url": "/royportfolio/static/css/main.a84aff85.chunk.css"
+  },
+  {
+    "revision": "64c0a03cc7f29a4bf030",
+    "url": "/royportfolio/static/js/main.64c0a03c.chunk.js"
+  },
+  {
+    "revision": "9ce7b4985150bacc00eb",
+    "url": "/royportfolio/static/js/runtime~main.9ce7b498.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/royportfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e48b95d231d256c063ad94baaaf559f1",
+    "url": "/royportfolio/index.html"
+  }
+];
